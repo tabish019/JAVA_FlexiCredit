9f33f1ea8a1dd9ba40bb7936d2ceeec087301945
tabish019 <tabishshahrozmansoori@gmail.com>
2026-04-21 11:22:43 +0530

    Add Student class with default constructor

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+	private int rollNo;
+	private String name;
+	private double fees;
+	
+	
+	public Student()  //Default Constructor
+	{
+		System.out.println(" Inside Default Constructor..");
+		
+	}
+}
